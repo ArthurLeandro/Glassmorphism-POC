ab4e391dd4d7012cf6dbb6d57a8b38aafbe7a6a1
ArthurLeandro <arthur.leandro11@gmail.com>
2021-05-12 19:49:50 -0300

    Projeto finalizado

---
+++ components/GlassCard.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <div class="glass glass-medium card">
+    <h3 class="card-title">Teste de GlassMorphism</h3>
+    <p>Fiquei encantado com o padrão e gostaria de saber como implementar.</p>
+    <a href="https://ui.glass" target="blank">Saiba mais</a>
+  </div>
+</div>
+</template>
+<style scoped>
+
+.card{
+  position:absolute;
+  top:50%;
+  left:25%;
+  text-align: center;
+  width: 400px;
+  height: auto;
+  padding: 2rem;
+  border-radius: 1rem;
+
+}
+
+.card-title {
+  margin-top: 0;
+  margin-bottom: .5rem;
+  font-size: 1.2rem;
+}
+
+p, a {
+  font-size: 1rem;
+}
+
+a {
+  color: #4d4ae8;
+  text-decoration: none;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+<!-- -->
+<div class="app">
+  <div class="Teste">
+  <GlassCard class="glass-back" style="top:10px; left:60px;"></GlassCard>
+  </div>
+  <vue-particles
+        color="#dedede"
+        :particleOpacity="0.7"
+        :particlesNumber="180"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#dedede"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      </vue-particles>
+</div>
+<!-- -->
+</template>
+
+<script>
+
+import GlassCard from "../components/GlassCard.vue";
+
+export default {
+  components:{
+    'GlassCard':GlassCard
+  }
+  
+}
+</script>
+
+<style lang="css">
+.app{
+  background: linear-gradient(191deg, rgba(5,17,78,1) 0%, rgb(68, 177, 122) 100%);
+}
+
+.Teste{
+  display: inline-block;
+}
+
+h1{
+  color:white;
+  text-align: center;
+  padding: 1rem;
+}
+</style>
